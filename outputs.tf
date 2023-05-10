4adbd8eecdc9909f4f2c1f22b5c786e3986af9f4
gundam-labs <112723653+gundam-labs@users.noreply.github.com>
2023-05-11 01:08:43 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,7 @@
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+#outputs.tf
+output "security_group_id" {
+  value       = aws_security_group.rds_sg.id
 }
-
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+output "db_instance_endpoint" {
+  value       = aws_db_instance.myinstance.endpoint
 }
-
