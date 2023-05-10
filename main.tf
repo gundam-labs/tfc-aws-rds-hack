2e827c65ea7a296dc419d47385e9d556330a6775
gundam-labs <112723653+gundam-labs@users.noreply.github.com>
2023-05-11 01:06:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@
 #defining the provider as aws
 provider "aws" {
     region     = "${var.region}"
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
+ 
 }
 
 #create a security group for RDS Database Instance
