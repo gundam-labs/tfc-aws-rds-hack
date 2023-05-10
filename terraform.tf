ea6054d5ced1ddb97622f3aadb6d6468ad1872fa
gundam-labs <112723653+gundam-labs@users.noreply.github.com>
2023-05-10 23:56:55 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 terraform {
 
   cloud {
-    organization = "organization-name"
+    organization = "hackCompany"
 
     workspaces {
-      name = "learn-terraform-cloud"
+      name = "rds-aws-hack"
     }
   }
 
